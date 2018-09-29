680be79c12e6944374c66a17b686a22608d75676
SenorGrande <clooneyjh@gmail.com>
2018-09-29 02:49:29 +0000

    Adding random AES key generation

--- client.py
+++ client.py
@@ -1,16 +1,33 @@
 import socket
 import sys
 from Crypto.PublicKey import RSA
-#import pickle
 from base64 import urlsafe_b64encode, urlsafe_b64decode
 from Crypto.Cipher import AES
 from Crypto import Random
+from Crypto.Random import random
+import string
 
+def keyGenAES(size=6, chars=string.ascii_letters + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+def encrypt(key, msg):
+    iv = Random.new().read(BS)
+    cipher = AES.new(key, AES.MODE_CFB, iv, segment_size=AES.block_size * 8)
+    encrypted_msg = cipher.encrypt(pad(str(msg)))
+    return base64unpad(urlsafe_b64encode(iv + encrypted_msg))
+
+def decrypt(key, msg):
+    decoded_msg = urlsafe_b64decode(base64pad(msg))
+    iv = decoded_msg[:BS]
+    encrypted_msg = decoded_msg[BS:]
+    cipher = AES.new(key, AES.MODE_CFB, iv, segment_size=AES.block_size * 8)
+    return unpad(cipher.decrypt(encrypted_msg))
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Connect the socket to the port where the server is listening
-server_address = ('35.231.58.65', 8090)
+#server_address = ('35.201.26.73', 8090) # AWS
+server_address = ('35.231.58.65', 8090) # GCP
 print >>sys.stderr, 'connecting to %s port %s' % server_address
 sock.connect(server_address)
 
@@ -37,20 +54,7 @@ unpad = lambda s: s[:-ord(s[len(s) - 1:])]
 
 base64pad = lambda s: s + '=' * (4 - len(s) % 4)
 base64unpad = lambda s: s.rstrip("=")
-encrypt_key = 'LKH1hb899Y09olUi' # generate a nonce???
-
-def encrypt(key, msg):
-    iv = Random.new().read(BS)
-    cipher = AES.new(key, AES.MODE_CFB, iv, segment_size=AES.block_size * 8)
-    encrypted_msg = cipher.encrypt(pad(str(msg)))
-    return base64unpad(urlsafe_b64encode(iv + encrypted_msg))
-
-def decrypt(key, msg):
-    decoded_msg = urlsafe_b64decode(base64pad(msg))
-    iv = decoded_msg[:BS]
-    encrypted_msg = decoded_msg[BS:]
-    cipher = AES.new(key, AES.MODE_CFB, iv, segment_size=AES.block_size * 8)
-    return unpad(cipher.decrypt(encrypted_msg))
+encrypt_key = keyGenAES(16)
 
 # Encrypt AES key
 enc_aes_key = rsa_key.encrypt(encrypt_key, 32)
